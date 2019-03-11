6950a7487403039a77a0f164285a293ccf35f732
Juho Vepsalainen <bebraw@gmail.com>
2019-03-11 11:29:02 +0100

    refactor: Replace graphql-code-generator with a small script

---
+++ utils/generate-introspection.js
@@ -0,0 +1,33 @@
+const fs = require("fs-extra");
+const prettier = require("prettier");
+const { buildSchema, introspectionFromSchema } = require("graphql");
+const mri = require("mri");
+const argv = process.argv.slice(2);
+
+// TODO: Expose input
+async function main() {
+  const args = mri(argv, {
+    default: {
+      verbose: false,
+    },
+    boolean: ["verbose"],
+  });
+  const { _: verbose } = args;
+  const schema = await fs.readFile("./server/types/schema.gql", {
+    encoding: "utf8",
+  });
+  const introspection = introspectionFromSchema(buildSchema(schema), {
+    descriptions: true,
+  });
+
+  const code = `const introspection = ${JSON.stringify(introspection)};
+export default introspection;`;
+
+  // TODO: Extract output file as a parameter. Also get cwd from process or argv.
+  await fs.writeFile(
+    "./server/types/schema.ts",
+    prettier.format(code, JSON.parse(await fs.readFile("./.prettierrc")))
+  );
+}
+
+main();
